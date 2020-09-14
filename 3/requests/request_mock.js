8bf7230bed29dd7e07732e15898089b098faaa52
Helen Dikareva <elena.dikareva@devexpress.com>
2020-09-14 09:34:21 +0300

    request_mock example

--- 3/requests/request_mock.js
+++ 3/requests/request_mock.js
@@ -1,1 +1,22 @@
-//TODO
+import { RequestMock, Selector } from 'testcafe';
+
+const weatherData = require('./weather.json');
+
+const mock = RequestMock()
+    .onRequestTo(new RegExp('https://api.openweathermap.org/data/2.5/weather'))
+    .respond(weatherData, 200, { 'Access-Control-Allow-Origin': '*' });
+
+fixture `Fixture`
+    .page('https://rawfour.github.io/weather-app-react/')
+    .requestHooks(mock);
+
+test('test', async t => {
+    const temperatureElement = Selector('h3');
+    const descriptionElement = Selector('h4').nth(1);
+
+    await t
+        .typeText('input[type=text]', 'New York')
+        .pressKey('enter')
+        .expect(temperatureElement.textContent).contains('30')
+        .expect(descriptionElement.textContent).eql('Clear sky');
+});
